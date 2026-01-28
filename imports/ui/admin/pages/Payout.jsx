805fd994556f341b464993c520760f6d59fc837b
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-01-28 20:49:18 +0100

    Create Payout.jsx
    Yes

---
+++ imports/ui/admin/pages/Payout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PayoutTable from '../components/PayoutTable';
+
+export default function Payouts() {
+  return (
+    <>
+      <h1>Payout Queue</h1>
+      <PayoutTable />
+    </>
+  );
+}
