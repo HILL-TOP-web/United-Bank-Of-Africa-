084405bd934f933795d7113b50d4a7beed1b2c91
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2026-01-28 20:51:43 +0100

    Create Login.jsx
    Yes

---
+++ imports/ui/admin/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Admin Login</h1>
+      <input placeholder="Email" /><br />
+      <input placeholder="Password" type="password" /><br />
+      <button>Login</button>
+    </div>
+  );
+}
